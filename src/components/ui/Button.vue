2b882822603620c51b80b6f3b49b7b9c7492e779
Sheraz <sherazarshad419@gmail.com>
2025-03-02 22:11:11 +0500

    custom ts tag enforce vite plgin and button with loading

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+console.log("Button");
+</script>
 <template>
-	<button class="bg-primary disabled:bg-primary/75 hover:bg-primary/75 flex items-center justify-center gap-2 rounded px-4 py-1.5 text-white transition duration-200 disabled:cursor-not-allowed">
-		<span>Large button</span>
+	<button class="bg-primary outline-primary disabled:bg-primary/75 hover:bg-primary/75 flex cursor-pointer items-center justify-center gap-2 rounded px-6 py-2 text-white transition duration-200 disabled:cursor-not-allowed" type="button">
+		<span>Button</span>
+		<!-- loading icon -->
+		<svg class="size-4 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" role="graphics-symbol" aria-labelledby="title-05 desc-05">
+			<title id="title-05">Icon title</title>
+			<desc id="desc-05">A more detailed description of the icon</desc>
+			<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+			<path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+		</svg>
 	</button>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+	plugins: [vue(), tailwindcss(), enforceScriptSetupLangTS()],
 });
+
+// ------------------------------- Custom Plugins -----------------------------------
+
+// Custom Vite plugin to enforce `lang="ts"` in `<script setup>`
+function enforceScriptSetupLangTS(): Plugin {
+	return {
+		name: "enforce-script-setup-lang-ts",
+		enforce: "pre",
+		transform(code, id) {
+			if (id.endsWith(".vue")) {
+				const hasScriptSetup = code.includes("<script setup");
+				const hasLangTS = code.includes('<script setup lang="ts"');
+
+				if (hasScriptSetup && !hasLangTS) {
+					throw new Error(`The file "${id}" is missing ---> (lang="ts") <--- in <script setup>.`);
+				}
+			}
+		},
+	};
+}
