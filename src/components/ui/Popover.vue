6327c737ff78d09b2e5f0d6445f3aeecb9f7692e
Sheraz <sherazarshad419@gmail.com>
2025-03-03 21:45:58 +0500

    tooltip completed and some tweaks in popover

--- src/components/ui/Popover.vue
+++ src/components/ui/Popover.vue
@@ -6,13 +6,14 @@ const isOpen = ref(false);
 defineProps({
 	position: { type: String as () => "left" | "center" | "right", default: "center" },
 	bodyClass: { type: String, default: "" },
+	contentClass: { type: String, default: "" },
 });
 </script>
 
 <template>
-	<div class="relative mx-auto w-fit pb-3" @mouseleave="isOpen = false">
+	<div class="relative" @mouseleave="isOpen = false">
 		<!-- component to render the popover on -->
-		<div class="relative w-fit" @mouseover="isOpen = true">
+		<div class="relative" @mouseover="isOpen = true">
 			<!-- arrow icon -->
 			<svg v-if="isOpen" class="absolute top-full left-1/2 w-4 -translate-x-1/2 text-gray-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 				<path fill="currentColor" d="M3 19h18a1.002 1.002 0 0 0 .823-1.569l-9-13c-.373-.539-1.271-.539-1.645 0l-9 13A.999.999 0 0 0 3 19" />
@@ -23,8 +24,10 @@ defineProps({
 		</div>
 
 		<!-- popover content -->
-		<div v-if="isOpen" :class="{ 'left-0': position === 'left', 'right-0': position === 'right', 'left-1/2 -translate-x-1/2': position === 'center', bodyClass }" class="absolute z-10 mt-3 w-[400px] rounded-md border border-gray-200 bg-white p-3 shadow-lg">
-			<slot name="body" />
+		<div v-if="isOpen" :class="{ 'left-0': position === 'left', 'right-0': position === 'right', 'left-1/2 -translate-x-1/2': position === 'center', bodyClass }" class="absolute z-10 pt-3">
+			<div :class="contentClass" class="w-[200px] rounded-md border border-gray-200 bg-white p-3 shadow-lg">
+				<slot name="body" />
+			</div>
 		</div>
 	</div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts">
 import Button from "../components/ui/Button.vue";
 import Popover from "../components/ui/Popover.vue";
+import Tooltip from "../components/ui/Tooltip.vue";
 </script>
 
 <template>
 	<div class="container py-8">
-		<Popover>
+		<div class="grid grid-cols-3 gap-4">
+			<!-- button -->
 			<Button label="Button" />
-			<template #body>
-				<div>Popover content</div>
-			</template>
-		</Popover>
+
+			<!-- Tooltip -->
+			<Tooltip>
+				<Button class="w-full" label="Hover me" />
+				<template #body>Tooltip content here</template>
+			</Tooltip>
+
+			<!-- popover -->
+			<Popover>
+				<Button class="w-full" label="Popover" />
+
+				<template #body>
+					<div>Popover content</div>
+				</template>
+			</Popover>
+		</div>
 	</div>
 </template>
