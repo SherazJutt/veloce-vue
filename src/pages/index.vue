717b3ad4c5cd82e4cc48b8ea24a9fda0b6baf84d
Sheraz <sherazarshad419@gmail.com>
2025-03-07 12:55:23 +0500

    added button variants

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -18,40 +18,45 @@ const selected = ref("Dashboard");
 				<Button variant="ghost" label="Ghost" />
 				<Button variant="outlined" label="Outlined" />
 				<Button variant="text" label="Text" />
+				<Button loading label="Loading" />
+				<Button icon="material-symbols:check" label="Icon" />
+				<Button icon="material-symbols:check" icon-position="left" label="Icon position" />
 			</div>
 		</div>
 
-		<div class="mb-4">
-			<h3 class="text-primary mb-2 font-semibold">Tooltip</h3>
-			<!-- Tooltip -->
-			<Tooltip>
-				<Button class="w-full" label="Hover me" />
-				<template #body>Tooltip content here</template>
-			</Tooltip>
-		</div>
+		<div class="grid grid-cols-4 gap-4">
+			<div class="mb-4">
+				<h3 class="text-primary mb-2 font-semibold">Tooltip</h3>
+				<!-- Tooltip -->
+				<Tooltip>
+					<Button class="w-full" label="Hover me" />
+					<template #body>Tooltip content here</template>
+				</Tooltip>
+			</div>
 
-		<div class="mb-4">
-			<h3 class="text-primary mb-2 font-semibold">popover</h3>
-			<!-- popover -->
-			<Popover>
-				<Button class="w-full" label="Popover" />
+			<div class="mb-4">
+				<h3 class="text-primary mb-2 font-semibold">popover</h3>
+				<!-- popover -->
+				<Popover>
+					<Button class="w-full" label="Popover" />
 
-				<template #body>
-					<div>Popover content</div>
-				</template>
-			</Popover>
-		</div>
+					<template #body>
+						<div>Popover content</div>
+					</template>
+				</Popover>
+			</div>
 
-		<div class="mb-4">
-			<h3 class="text-primary mb-2 font-semibold">Select</h3>
-			<Select v-model="selected" :options="['Dashboard', 'Analytics', 'Settings', 'Profile']" />
-		</div>
+			<div class="mb-4">
+				<h3 class="text-primary mb-2 font-semibold">Select</h3>
+				<Select v-model="selected" :options="['Dashboard', 'Analytics', 'Settings', 'Profile']" />
+			</div>
 
-		<div class="mb-4">
-			<h3 class="text-primary mb-2 font-semibold">Modal</h3>
-			<Modal>
-				<Button class="w-full" label="Modal" />
-			</Modal>
+			<div class="mb-4">
+				<h3 class="text-primary mb-2 font-semibold">Modal</h3>
+				<Modal>
+					<Button class="w-full" label="Modal" />
+				</Modal>
+			</div>
 		</div>
 	</div>
 </template>
