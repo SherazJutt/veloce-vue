8ba5601c1166313de0ca23a75beb2a6886759688
Sheraz <sherazarshad419@gmail.com>
2025-03-17 14:53:58 +0500

    added search option to select

--- src/components/ui/Select.vue
+++ src/components/ui/Select.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import Button from "./Button.vue";
 
 const props = defineProps({
@@ -10,7 +10,7 @@ const props = defineProps({
 
 const emit = defineEmits(["update:modelValue"]);
 
-const isOpen = ref(false);
+const isOpen = ref(true);
 const selectedOption = ref(props.modelValue);
 
 const selectOption = (option: string) => {
@@ -18,6 +18,14 @@ const selectOption = (option: string) => {
 	emit("update:modelValue", option);
 	isOpen.value = false;
 };
+
+const searchQuery = ref("");
+
+// Computed for filtering options based on search input
+const filteredOptions = computed(() => {
+	if (!searchQuery.value) return props.options;
+	return props.options.filter((option) => option.toLowerCase().includes(searchQuery.value.toLowerCase()));
+});
 </script>
 
 <template>
@@ -27,7 +35,10 @@ const selectOption = (option: string) => {
 
 		<!-- select menu -->
 		<ul v-if="isOpen" class="absolute top-full z-10 mt-1 flex w-full list-none flex-col rounded border border-gray-200 bg-white shadow-md shadow-slate-500/10">
-			<li v-for="(option, index) in options" :key="index" :class="option === selectedOption && 'bg-primary/10'" class="hover:bg-primary/5 cursor-pointer px-4 py-2" @click="selectOption(option)">
+			<li class="border-b border-gray-200 p-2">
+				<input type="text" v-model="searchQuery" class="focus:border-primary w-full rounded-sm border border-gray-200 px-2 py-1 focus:outline-none" placeholder="Search" />
+			</li>
+			<li v-for="(option, index) in filteredOptions" :key="index" :class="option === selectedOption && 'bg-primary/10'" class="hover:bg-primary/5 cursor-pointer px-4 py-2" @click="selectOption(option)">
 				{{ option }}
 			</li>
 		</ul>
