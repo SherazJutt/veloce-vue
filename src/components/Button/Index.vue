58a2ec4077ae6f73fa928b2203cdb339baf64dcf
Sheraz Jutt <sherazarshad419@gmail.com>
2025-10-10 00:36:10 +0500

    Update Button component styles for improved disabled state handling
    - Enhanced button styles to ensure consistent appearance when disabled across all variants.
    - Adjusted computed class logic to include disabled state styles for outlined, text, ghost, and solid variants.
    - Updated template class bindings to reflect changes in disabled opacity for better user experience.

--- src/components/Button/Index.vue
+++ src/components/Button/Index.vue
@@ -8,13 +8,13 @@ const props = defineProps(buttonProps);
 const classes = computed(() => {
   switch (props.variant) {
     case "outlined":
-      return "border border-primary text-primary bg-transparent hover:bg-primary hover:text-white";
+      return "!border-primary text-primary bg-transparent hover:bg-primary disabled:hover:bg-transparent disabled:hover:text-primary hover:text-white";
     case "text":
-      return "bg-transparent text-primary hover:bg-gray-200";
+      return "bg-transparent text-primary hover:bg-gray-200 disabled:hover:bg-transparent disabled:hover:text-primary";
     case "ghost":
-      return "text-primary bg-gray-100 hover:bg-gray-200";
+      return "text-primary bg-gray-100 hover:bg-gray-200 disabled:hover:bg-transparent disabled:hover:text-primary";
     default:
-      return "bg-primary text-white hover:bg-primary/75"; // solid
+      return "bg-primary text-white hover:bg-primary/75 disabled:hover:bg-primary disabled:hover:text-white"; // solid
   }
 });
 </script>
@@ -26,7 +26,7 @@ const classes = computed(() => {
     :disabled="props.disabled || props.loading" 
     :class="[
       classes, { 'rounded-full': props.rounded }, 
-      'flex cursor-pointer items-center justify-center gap-2 rounded border border-transparent px-3 py-2 transition duration-200 disabled:cursor-not-allowed disabled:opacity-50'
+      'flex cursor-pointer items-center justify-center gap-2 rounded border border-transparent px-3 py-2 transition duration-200 disabled:cursor-not-allowed disabled:opacity-75 '
     ]"
   >
     <div :class="{ 'justify-center': !props.icon }" class="flex w-full items-center justify-between gap-2">
