59fcd3fba1ec61cf52f5318a99f054a0cba8765c
Sheraz Jutt <sherazarshad419@gmail.com>
2025-10-12 14:05:49 +0500

    added chceckbox component

--- src/components/Checkbox/Index.vue
+++ src/components/Checkbox/Index.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-defineProps({
-  label: { type: String, default: "" },
-  labelClass: { type: String, default: "" },
-  checkboxClass: { type: String, default: "" },
-});
+import { props } from "./props";
+import Icon from "../icon/index.vue";
+
+defineProps(props);
+
 const modelValue = defineModel();
 </script>
 
 <template>
   <div class="flex w-fit cursor-pointer gap-2 select-none" @click="modelValue = !modelValue">
     <div :class="[{ 'bg-primary': modelValue }, checkboxClass]" class="border-primary m-0 size-6 rounded-sm border-2 duration-200">
-      <Icon v-if="modelValue" icon="material-symbols:check-rounded" :class="{ 'text-white': modelValue }" class="size-5" />
+      <Icon v-if="modelValue" icon="check" :class="{ 'text-white': modelValue }" class="size-5" />
     </div>
     <div :class="labelClass">{{ label }}</div>
   </div>
---
+++ src/components/Checkbox/props.ts
@@ -0,0 +1,5 @@
+export const props = {
+  label: { type: String, default: "" },
+  labelClass: { type: String, default: "" },
+  checkboxClass: { type: String, default: "" },
+} as const;
