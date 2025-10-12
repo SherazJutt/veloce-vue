f7fa239f5b2ae47dc68730996dcdf4155b8e2205
Sheraz Jutt <sherazarshad419@gmail.com>
2025-10-12 12:25:17 +0500

    Update Home page layout and add theme color display
    - Changed the main heading from "Just Hello" to "Welcome".
    - Introduced a new section for displaying theme colors with corresponding styles.
    - Added Tailwind CSS styles for the theme colors section to enhance layout and interactivity.

--- src/pages/Home/Index.vue
+++ src/pages/Home/Index.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <h1>Just Hello</h1>
+    <h1 class="py-6 text-center text-3xl font-bold">Welcome</h1>
+
+    <div class="mt-3">
+      <h3 class="text-lg font-medium">Theme Colors</h3>
+
+      <div class="theme-colors mt-2.5">
+        <div :style="{ backgroundColor: 'var(--ui-color-primary)' }">Primary color</div>
+        <div :style="{ backgroundColor: 'var(--ui-color-secondary)' }">Secondary color</div>
+        <div :style="{ backgroundColor: 'var(--ui-color-accent)' }">Accent color</div>
+      </div>
+    </div>
   </div>
 </template>
+<style>
+@reference "tailwindcss";
+
+.theme-colors {
+  @apply flex flex-wrap gap-2 text-center [&>*]:rounded-md [&>*]:px-6 [&>*]:py-2.5 [&>*]:transition-opacity [&>*]:duration-300;
+  @apply [&>*:hover]:cursor-pointer [&>*:hover]:opacity-75;
+}
+</style>
