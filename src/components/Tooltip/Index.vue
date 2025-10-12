4a9df85f5919c1121a1dbd803c06a2e8ccf251e3
Sheraz Jutt <sherazarshad419@gmail.com>
2025-10-12 14:46:24 +0500

    Implement Tooltip component with customizable positioning and content
    - Added a new Tooltip component with props for content and position (top, bottom, left, right).
    - Created stories for the Tooltip component in Storybook, showcasing different positions and default content.
    - Removed the old Tooltip story file to streamline the component structure.

--- src/components/Tooltip/Index.vue
+++ src/components/Tooltip/Index.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { props } from "./props";
 
-const isOpen = ref(false);
+const isVisible = ref(false);
 
-defineProps({
-  position: { type: String as () => "left" | "top" | "right" | "bottom", default: "bottom" },
-  bodyClass: { type: String, default: "" },
-});
+defineProps(props);
 </script>
 
 <template>
-  <div class="relative" @mouseenter="isOpen = true" @mouseleave="isOpen = false">
+  <div class="relative inline-block" @mouseenter="isVisible = true" @mouseleave="isVisible = false">
     <!-- Tooltip trigger -->
     <slot />
 
     <!-- Tooltip content -->
     <!-- prettier-ignore -->
     <div 
-		v-if="isOpen" 
+		v-if="isVisible" 
 		:class="[
 			'absolute z-10 w-max rounded-md bg-white px-3 py-2 text-sm text-primary border shadow-xl transition-opacity duration-200',
 			position === 'top' && 'bottom-full left-1/2 mb-2 -translate-x-1/2',
@@ -26,7 +24,8 @@ defineProps({
 			position === 'right' && 'top-1/2 left-full ml-2 -translate-y-1/2',
 			bodyClass
 		]">
-		<slot name="body" />
+    <span v-if="!content">{{ content }}</span>
+		<slot name="body" v-else />
 		</div>
   </div>
 </template>
