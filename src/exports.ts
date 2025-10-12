e5be7d1762b3ee747485815faa431c83f7749832
Sheraz Jutt <sherazarshad419@gmail.com>
2025-10-13 01:44:11 +0500

    Add new components to exports
    - Introduced Drawer, Modal, RangeSlider, Select, and Separator components to the exports.
    - Retained the Tooltip component while ensuring all components are accessible for streamlined usage.

--- src/exports.ts
+++ src/exports.ts
@@ -1,8 +1,13 @@
 export { default as Accordion } from "./components/Accordion/Index.vue";
 export { default as Button } from "./components/Button/Index.vue";
 export { default as Checkbox } from "./components/Checkbox/Index.vue";
+export { default as Drawer } from "./components/Drawer/Index.vue";
 export { default as Input } from "./components/Input/Index.vue";
-export { default as Tooltip } from "./components/Tooltip/Index.vue";
+export { default as Modal } from "./components/Modal/Index.vue";
 export { default as Popover } from "./components/Popover/Index.vue";
+export { default as RangeSlider } from "./components/RangeSlider/Index.vue";
+export { default as Select } from "./components/Select/Index.vue";
+export { default as Separator } from "./components/Separator/Index.vue";
+export { default as Tooltip } from "./components/Tooltip/Index.vue";
 
 export { config } from "./utils/config";
