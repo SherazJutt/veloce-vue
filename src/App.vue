0e9692b6796692f0e1ec4a3e59732eda57934402
Sheraz Jutt <sherazarshad419@gmail.com>
2025-10-15 09:03:42 +0500

    book adde

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import Button from "./components/button/Index.vue";
 </script>
 <template>
   <RouterView />
-  <Button label="Hello" variant="outlined" />
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/", component: () => import("../pages/Home/Index.vue") }],
+  routes: [
+    { path: "/", component: () => import("../pages/home/Index.vue") },
+    { path: "/book", component: () => import("../pages/book/Index.vue") },
+  ],
 });
 
 export default router;
