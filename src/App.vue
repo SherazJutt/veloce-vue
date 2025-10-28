0150800013e27b1531df71fdcd3295c3e2480a07
Sheraz Jutt <sherazarshad419@gmail.com>
2025-10-29 02:32:36 +0500

    Initialize Veloce Vue project with basic structure
    - Created index.html as the entry point for the application.
    - Set up initial Vue application in index.ts, mounting the App component.
    - Added App.vue with a simple "Hello World" template and a Button component.
    - Removed Storybook configuration files and stories to streamline the project setup.
    - Updated package.json to replace Storybook with Vite for development.

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Button } from "./ui/button";
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello World</h1>
+    <Button>Click me</Button>
+  </div>
+</template>
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 import "./style.css";
 export * from "./exports";
+import App from "./App.vue";
+import { createApp } from "vue";
+
+const app = createApp(App);
+app.mount("#app");
