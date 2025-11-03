8b2502707102d9894368de7bedf6baa34670fe9c
Sheraz Jutt <sherazarshad419@gmail.com>
2025-11-03 17:47:32 +0500

    Update HomePage button icon and introduce Icon component in UI package
    - Changed button icon in HomePage from alert to loading for better user feedback.
    - Added new Icon component to render various icons dynamically.
    - Created iconsMap to manage icon imports and enhance accessibility across components.

--- apps/playground/src/pages/HomePage.vue
+++ apps/playground/src/pages/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="my-4 text-center text-2xl font-semibold">Components</h1>
-    <Button icon="alert" variant="outline">Click me</Button>
+    <Button icon="loading" variant="outline">Click me</Button>
   </div>
 </template>
 <script setup lang="ts">
--- packages/ui/src/components/icon/index.ts
+++ packages/ui/src/components/icon/index.ts
@@ -1,23 +1,9 @@
 import type { Component } from "vue";
-import Check from "../../icons/Check.vue";
-import Close from "../../icons/Close.vue";
-import Loading from "../../icons/Loading.vue";
-import ChevronDown from "../../icons/ChevronDown.vue";
-import Alert from "../../icons/Alert.vue";
-import Hamburger from "../../icons/Hamburger.vue";
-
-// Map of all icons
-const iconsMap = {
-  check: Check,
-  close: Close,
-  loading: Loading,
-  "chevron-down": ChevronDown,
-  alert: Alert,
-  hamburger: Hamburger,
-} as const;
+import { iconsMap } from "./icons";
 
 // icons names for autocomplete
 export type Icons = keyof typeof iconsMap;
 export const Icons: Record<Icons, Component> = iconsMap;
 export const iconsList = Object.keys(iconsMap) as Icons[];
-export { default as Icon } from "./index.vue";
+
+export { default as Icon } from "./Icon.vue";
