71640674e3c81008a31a3930aa936d0a67524ecd
Sheraz Jutt <sherazarshad419@gmail.com>
2025-11-13 03:34:50 +0500

    Enhance Accordion component with additional features and examples
    - Updated Accordion.vue to support new props: `bordered` and `rounded`, allowing for customizable styling options.
    - Added multiple examples in Accordion.vue to demonstrate various configurations, including default, rounded, shadow, multiple open items, and no border.
    - Introduced a new `MultipleOpenItems` array to showcase the functionality of multiple open accordions, improving usability and visual clarity.

--- apps/playground/src/pages/components/Accordion.vue
+++ apps/playground/src/pages/components/Accordion.vue
@@ -1,8 +1,24 @@
 <template>
-  <Accordion :items="items" />
+  <div class="pb-16">
+    <H4 text="Default Accordion" margin-bottom="md" />
+    <Accordion :items="items" />
+
+    <H4 text="Rounded none" margin-bottom="md" />
+    <Accordion :items="items" rounded="none" />
+
+    <H4 text="shadow" margin-bottom="md" />
+    <Accordion :items="items" shadow />
+
+    <H4 text="Multiple Open" margin-bottom="md" />
+    <Accordion :items="MultipleOpenItems" :multiple="true" />
+
+    <H4 text="No Border" margin-bottom="md" />
+    <Accordion :items="items" :bordered="false" />
+  </div>
 </template>
 <script setup lang="ts">
 import { Accordion } from "@veloce/ui";
+import { H4 } from "@veloce/typography";
 import { type AccordionItem } from "@veloce/types";
 
 const items: AccordionItem[] = [
@@ -15,6 +31,26 @@ const items: AccordionItem[] = [
     title: "Item 2",
     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, eos! Voluptas explicabo odit quis earum nobis! Totam, molestias, magni beatae illum assumenda ipsum quas a nemo, tenetur perferendis minima quis!",
     icon: "sun",
+  },
+  {
+    title: "Item 3",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, eos! Voluptas explicabo odit quis earum nobis! Totam, molestias, magni beatae illum assumenda ipsum quas a nemo, tenetur perferendis minima quis!",
+    icon: "hamburger",
+  },
+];
+
+const MultipleOpenItems: AccordionItem[] = [
+  {
+    title: "Item 1",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, eos! Voluptas explicabo odit quis earum nobis! Totam, molestias, magni beatae illum assumenda ipsum quas a nemo, tenetur perferendis minima quis!",
+    icon: "moon",
+    active: true,
+  },
+  {
+    title: "Item 2",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, eos! Voluptas explicabo odit quis earum nobis! Totam, molestias, magni beatae illum assumenda ipsum quas a nemo, tenetur perferendis minima quis!",
+    icon: "sun",
+    active: true,
   },
   {
     title: "Item 3",
