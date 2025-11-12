d5fc191e25602ba293323b626e2ac92b2bd77214
Sheraz Jutt <sherazarshad419@gmail.com>
2025-11-13 03:14:59 +0500

    Refactor Tooltip and Modal components for improved layout and responsiveness
    - Updated Tooltip.vue to enhance layout by introducing a grid structure for better organization of tooltip buttons.
    - Modified Modal.vue to ensure the backdrop covers the full screen on larger devices, improving user experience and accessibility.

--- apps/playground/src/pages/components/Tooltip.vue
+++ apps/playground/src/pages/components/Tooltip.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="flex size-full items-center justify-center gap-4">
-    <Tooltip content="Tooltip Content" position="top">
-      <Button label="Tooltip Top" severity="neutral" />
-    </Tooltip>
+  <div class="flex size-full items-center justify-center">
+    <div class="grid gap-4 max-md:w-3/4 max-md:max-w-[320px] md:grid-cols-2 lg:grid-cols-4">
+      <Tooltip content="Tooltip Content" position="top">
+        <Button class="w-full" label="Tooltip Top" severity="neutral" />
+      </Tooltip>
 
-    <Tooltip content="Tooltip Content" position="bottom">
-      <Button label="Tooltip Bottom" severity="secondary" />
-    </Tooltip>
+      <Tooltip content="Tooltip Content" position="bottom">
+        <Button class="w-full" label="Tooltip Bottom" severity="secondary" />
+      </Tooltip>
 
-    <Tooltip content="Tooltip Content" position="left">
-      <Button label="Tooltip Left" severity="success" />
-    </Tooltip>
+      <Tooltip content="Tooltip Content" position="left">
+        <Button class="w-full" label="Tooltip Left" severity="success" />
+      </Tooltip>
 
-    <Tooltip content="Tooltip Content" position="right">
-      <Button label="Tooltip Right" severity="primary" />
-    </Tooltip>
+      <Tooltip content="Tooltip Content" position="right">
+        <Button class="w-full" label="Tooltip Right" severity="primary" />
+      </Tooltip>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
