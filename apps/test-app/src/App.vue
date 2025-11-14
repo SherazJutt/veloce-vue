eeab9b8194fec56aca59ef6a22435fb6c2fa64e4
Sheraz Jutt <sherazarshad419@gmail.com>
2025-11-15 00:50:19 +0500

    Enhance App.vue with Badge component integration
    - Added a Badge component to the App.vue file, improving the UI by providing additional visual feedback.
    - Included the necessary import statement for the Badge component from the veloce-vue/ui library.
    - Cleaned up commented-out template sections for better code readability.

--- apps/test-app/src/App.vue
+++ apps/test-app/src/App.vue
@@ -1,5 +1,31 @@
-<template>
+<!-- <template>
   <div>
     <h1 class="text-2xl font-bold underline">Hello World</h1>
   </div>
+</template> -->
+
+<!-- <template>
+  <div>
+    <h1 class="text-2xl font-bold underline">Hello World</h1>
+    <Icon icon="at-sign" />
+  </div>
 </template>
+<script setup lang="ts">
+import { Icon } from "veloce-vue/icons";
+</script> -->
+
+<!-- <template>
+  <div>
+    <h1 class="text-2xl font-bold underline">Hello World</h1>
+  </div>
+</template> -->
+
+<template>
+  <div>
+    <h1 class="text-2xl font-bold underline">Hello World</h1>
+    <Badge label="Badge" />
+  </div>
+</template>
+<script setup lang="ts">
+import { Badge } from "veloce-vue/ui";
+</script>
