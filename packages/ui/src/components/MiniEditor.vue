8c49890a8a91ac9750857b81f9bdd150f9cd22e1
Sheraz Jutt <sherazarshad419@gmail.com>
2025-12-20 20:08:44 +0500

    Refactor MiniEditor.vue to remove unused imports
    - Removed unused imports of defineProps and defineEmits from MiniEditor.vue to clean up the code.

--- packages/ui/src/components/MiniEditor.vue
+++ packages/ui/src/components/MiniEditor.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch, onMounted, defineProps, defineEmits } from "vue";
+import { ref, watch, onMounted } from "vue";
 import Button from "./Button.vue";
 import { Bold, Italic, Highlight } from "@veloce-vue/icons";
 
