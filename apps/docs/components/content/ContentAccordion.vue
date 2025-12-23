6df0fefc3cdc1dd6cf6ead3701e42752161ff0c8
Sheraz Jutt <sherazarshad419@gmail.com>
2025-12-23 20:09:47 +0500

    Enhance ContentAccordion component and update documentation
    - Added support for multiple rounded styles in the ContentAccordion component.
    - Updated the Accordion documentation to include new usage examples for different rounded types and shadow options.
    - Improved CSS styles for first-child margin and padding adjustments.
    - Updated contents.sqlite to reflect the latest changes in the Accordion component and documentation.

--- apps/docs/components/content/ContentAccordion.vue
+++ apps/docs/components/content/ContentAccordion.vue
@@ -1,9 +1,19 @@
 <template>
   <Accordion v-if="type === 'default'" :items="itemsContent" />
-  <Accordion v-if="type === 'rounded-none'" :items="items" rounded="none" />
+
+  <Accordion v-if="type === 'rounded-none'" :items="itemsContent" rounded="none" />
+  <Accordion v-if="type === 'rounded-sm'" :items="itemsContent" rounded="sm" />
+  <Accordion v-if="type === 'rounded-md'" :items="itemsContent" rounded="md" />
+  <Accordion v-if="type === 'rounded-lg'" :items="itemsContent" rounded="lg" />
+  <Accordion v-if="type === 'rounded-xl'" :items="itemsContent" rounded="xl" />
+
   <Accordion v-if="type === 'shadow'" :items="itemsContent" shadow />
+  <Accordion v-if="type === 'single'" :items="SingleOpenItem" />
   <Accordion v-if="type === 'multiple'" :items="MultipleOpenItems" :multiple="true" />
-  <Accordion v-if="type === 'bordered'" :items="itemsContent" :bordered="false" />
+  <Accordion v-if="type === 'without-border'" :items="itemsContent" :bordered="false" />
+  <Accordion v-if="type === 'with-border'" :items="itemsContent" :bordered="true" />
+  <Accordion v-if="type === 'with-icons'" :items="items" />
+  <Accordion v-if="type === 'custom-classes'" contentClass="text-blue-600 font-semibold" headerClass="text-purple-600" :items="itemsContent" />
 </template>
 <script setup lang="ts">
 import { Accordion } from "veloce-vue/ui";
@@ -12,7 +22,7 @@ import { Moon, Sun, Hamburger } from "veloce-vue/icons";
 
 const props = defineProps({
   type: {
-    type: String as () => "default" | "rounded-none" | "shadow" | "multiple" | "bordered",
+    type: String as () => "default" | "rounded-none" | "rounded-sm" | "rounded-md" | "rounded-lg" | "rounded-xl" | "shadow" | "single" | "multiple" | "without-border" | "with-border" | "with-icons" | "custom-classes" | "shadow-multiple",
     required: false,
   },
 });
@@ -37,9 +47,15 @@ const items: AccordionItem[] = itemsContent.map((item, index) => ({
   icon: [Moon, Sun, Hamburger][index],
 }));
 
+const SingleOpenItem: AccordionItem[] = itemsContent.map((item, index) => ({
+  ...item,
+  icon: [Moon, Sun, Hamburger][index],
+  active: index === 0,
+}));
+
 const MultipleOpenItems: AccordionItem[] = itemsContent.map((item, index) => ({
   ...item,
   icon: [Moon, Sun, Hamburger][index],
-  active: index < 2,
+  active: index === 0 || index === 2,
 }));
 </script>
